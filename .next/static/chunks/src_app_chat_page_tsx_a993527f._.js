8c25a9733b71cfdd08e54057161a67549924c425
Hitendrasinh <hitendrasinh2004@gmail.com>
2025-06-17 19:15:13 +0530

    Add .next/static/chunks/src_app_chat_page_tsx_a993527f._.js 1750167912899

---
+++ .next/static/chunks/src_app_chat_page_tsx_a993527f._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/node_modules_a24d0ab3._.js",
+  "static/chunks/src_06952097._.js"
+],
+    source: "dynamic"
+});
